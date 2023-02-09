7fcb7e8f5565016e517fde791e4ee8ebb3dcd589
Team25 <ase.team25@gmail.com>
2023-02-09 18:41:40 -0500

    UI changes

--- Winnocare/src/app/register/personal-details/personal-details.component.ts
+++ Winnocare/src/app/register/personal-details/personal-details.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { ToastController } from '@ionic/angular';
+import { ToastPosition } from '@ionic/core';
 import * as countries from '../../../assets/json/countries.json';
 
 @Component({
@@ -11,22 +13,33 @@ export class PersonalDetailsComponent implements OnInit {
 
   name: any;
   age: any;
-  gender:any;
-  country:any;
-  contact:any;
-  countryList:{name:string, dial_code: string, code:string}[] = [];
- 
-  constructor(private router: Router, private route: ActivatedRoute) { }
+  gender: any;
+  country: any;
+  contact: any;
+  countryList: { name: string, dial_code: string, code: string }[] = [];
+
+  constructor(private router: Router, private route: ActivatedRoute, private toastController: ToastController) { }
 
   ngOnInit() {
-    // fetch('assets/countries.json').then(res => res.json())
-    // .then(jsonData => {
-    //   this.countryList = jsonData;
-    // });
+    fetch('assets/json/countries.json').then(res => res.json())
+      .then(jsonData => {
+        this.countryList = jsonData;
+      });
   }
 
   next() {
     this.router.navigate(['register/emergencyContacts']);
+
+
+  }
+
+  async showToast(position: ToastPosition, message: string) {
+    const toast = await this.toastController.create({
+      message: message,
+      duration: 1500,
+      position: position
+    });
+    await toast.present();
   }
 
 }

d115b967a4782314784ab2bb714fe4ad870ac414
Team25 <ase.team25@gmail.com>
2023-02-18 09:28:55 -0500

    changes to forgot password screen

--- Winnocare/src/app/forgot-password/forgot-password.component.ts
+++ Winnocare/src/app/forgot-password/forgot-password.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { CommonService } from 'src/app/services/common.service';
+import { ToastService } from '../services/toast.service';
+
+
 
 @Component({
   selector: 'app-forgot-password',
@@ -7,8 +13,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ForgotPasswordComponent implements OnInit {
 
-  constructor() { }
+  forgotPasswordForm: FormGroup;
+  constructor(private router: Router,
+    private route: ActivatedRoute,
+    private fb: FormBuilder,
+    private commonService: CommonService,private toastService: ToastService) { }
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.forgotPasswordForm = this.fb.group({
+      oldPassword: ['', Validators.required],
+      newPassword: ['', Validators.required],
+      confirmNewPassword: ['', Validators.required]
+    },this.passwordMatch('newPassword', 'confirmNewPassword') );
+  }
+
+  passwordMatch(password: string, confirmPassword: string) {
+    return (formGroup: FormGroup) => {
+      if(password !== confirmPassword)
+      {
+        this.toastService.showToast('bottom', 'Login Failed. Please check username and password.');
+
+      }
+         return null
+     }
+}
+
+
+  validateForm(form: FormGroup) {
+    if (form.valid) {
+      //this.router.navigate(['forgotPassword', { registrationDetails: JSON.stringify(user) }]);
+    } else {
+
+      this.commonService.validateAllFormFields(form);    }
+  }
 
 }
